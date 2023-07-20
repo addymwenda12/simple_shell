fedf3c2a2d025b40830295f488cf6211c00dcbeb
addymwenda12 <muneneaddy@gmail.com>
2023-07-20 01:39:52 -0700

    Shell function

--- command_separator.c
+++ command_separator.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * command_separator - Use to separate commands by ;
+ * command_separator - Use to separate commands by ;, &&, ||
  * @cmd: Command string to be used
  *
  * Return: The separated commands
@@ -19,13 +19,13 @@ char **command_separator(char *cmd)
 		exit(EXIT_FAILURE);
 	}
 
-	command = strtok(cmd, ";");
+	command = strtok(cmd, ";&&||");
 
 	while (command != NULL)
 	{
 		commands[i] = command;
 		i++;
-		command = strtok(NULL, ";");
+		command = strtok(NULL, ";&&||");
 	}
 	commands[i] = NULL;
 
